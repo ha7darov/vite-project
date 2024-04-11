b480efaaca5af3a8089b6c0540a51a9b289f8cbc
ha7darov <vi.yeoun.vi@gmail.com>
2024-04-12 00:52:58 +0500

    Movie added

---
+++ movie/src/components/app-filter/AppFilter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>App Filter</h1>        
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+    
+</style>
---
+++ movie/src/components/movie-add-form/MovieAddForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Movie add form</h1>        
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+    
+</style>
